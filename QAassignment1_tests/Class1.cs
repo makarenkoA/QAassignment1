234329d65d7ac4eba5786a230faf1ee4a709790e
Anastasiia Makarenko <nastya.makarenko272@gmail.com>
2019-02-01 08:38:17 -0500

    n

---
+++ QAassignment1_tests/Class1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QAassignment1_tests;
+
+namespace QAassignment1_tests
+{ 
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+
+        }
+    }
+}
